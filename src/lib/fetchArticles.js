7876ec15dc251b59cf414bfc8eb02b4e4dea422d
minepax <maherbecs@gmail.com>
2025-12-10 17:47:07 +0400

    Hmmmm

--- src/lib/fetchArticles.js
+++ src/lib/fetchArticles.js
@@ -8,8 +8,16 @@ const myWixClient = createClient({
     }),
 });
 
-export default async function fetchArticles({collection, count} = {}) {
+export default async function fetchArticles({collection, count, categoryName, slug} = {}) {
     let response = await myWixClient.items.query(collection);
+
+    if (categoryName) {
+        response = response.eq('category', categoryName);
+    }
+
+    if (slug) {
+        response = response.eq('slug', slug);
+    }
 
     if(count != null) {
         response = response.limit(count);
