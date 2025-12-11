a6813ea4a4e8609894ffd5480c12e793b4387400
minepax <maherbecs@gmail.com>
2025-12-11 11:51:35 +0400

    Everything working

--- src/lib/fetchArticles.js
+++ src/lib/fetchArticles.js
@@ -1,14 +1,15 @@
 import { createClient, OAuthStrategy } from '@wix/sdk';
 import { items } from '@wix/data';
 
+const WIX_CLIENT_ID = process.env.VITE_WIX_CLIENT_ID;
+
 const myWixClient = createClient({
     modules: { items },
-    auth: OAuthStrategy({
-        clientId: process.env.WIX_CLIENT_ID,
-    }),
+    auth: OAuthStrategy( {clientId: WIX_CLIENT_ID,} ),
 });
 
 export default async function fetchArticles({collection, count, categoryName, slug} = {}) {
+
     let response = await myWixClient.items.query(collection);
 
     if (categoryName) {
