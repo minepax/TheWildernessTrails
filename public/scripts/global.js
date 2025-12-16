9d9afc6600f1a9e63e1f909012dbb3c81f24debe
minepax <maherbecs@gmail.com>
2025-12-16 11:14:41 +0400

    Added astro view transitions :)

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,6 +1,11 @@
-const menuBtn = document.querySelector('.hamburger-btn');
-const nav = document.querySelector('.article-nav');
+function initMenu() {
+    const menuBtn = document.querySelector('.hamburger-btn');
+    const nav = document.querySelector('.article-nav');
+    
+    menuBtn.addEventListener('click', () => {
+        nav.toggleAttribute("data-visible");
+    });
+}
 
-menuBtn.addEventListener('click', () => {
-    nav.toggleAttribute("data-visible");
-});
+initMenu()
+document.addEventListener("astro:after-swap", initMenu)
